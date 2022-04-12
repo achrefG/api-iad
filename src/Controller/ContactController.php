19da82cdd682228a2dc169c5dd8068be32b143f7
achref ghezil <achref.ghezil@yahoo.com>
2022-04-12 03:21:16 +0200

    PUT Request : UPDATE A CONTACT

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -22,7 +22,6 @@ class ContactController extends AbstractController
         $contacts = $this->getDoctrine()
             ->getRepository(Contact::class)
             ->findAll();
-        dump($contacts);
         $data = [];
 
         foreach ($contacts as $contact) {
@@ -41,7 +40,7 @@ class ContactController extends AbstractController
 
     #------------POST Request----CREATE NEW CONTACT------------------------
     /**
-     * @Route("/project", name="project_new", methods={"POST"})
+     * @Route("/contact", name="contact_new", methods={"POST"})
      */
     public function new(Request $request): Response
     {
@@ -59,5 +58,64 @@ class ContactController extends AbstractController
         $entityManager->flush();
 
         return $this->json('Created new project successfully with id ' . $contact->getId());
+    }
+    #------------GET Request----GET CONTACT BY ID------------------------
+    /**
+     * @Route("/contact/{id}", name="contact_show", methods={"GET"})
+     */
+    public function show(int $id): Response
+    {
+        $contact = $this->getDoctrine()
+            ->getRepository(Contact::class)
+            ->find($id);
+
+        if (!$contact) {
+
+            return $this->json('No project found for id :' . $id, 404);
+        }
+
+        $data =  [
+            'id' => $contact->getId(),
+            'name' => $contact->getName(),
+            'firstName' => $contact->getFirstName(),
+            'email' => $contact->getEmail(),
+            'address' => $contact->getAddress(),
+            'phone' => $contact->getPhone(),
+            'age' => $contact->getAge(),
+        ];
+
+        return $this->json($data);
+    }
+    #------------PUT Request----UPDATE A CONTACT------------------------
+    /**
+     * @Route("/contact/{id}", name="contact_edit", methods={"PUT"})
+     */
+    public function edit(Request $request, int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $contact = $entityManager->getRepository(Contact::class)->find($id);
+
+        if (!$contact) {
+            return $this->json('No project found for id :' . $id, 404);
+        }
+        $contact->setName($request->request->get('name'));
+        $contact->setFirstName($request->request->get('firstName'));
+        $contact->setEmail($request->request->get('email'));
+        $contact->setAddress($request->request->get('address'));
+        $contact->setPhone($request->request->get('phone'));
+        $contact->setAge($request->request->get('age'));
+        $entityManager->flush();
+
+        $data =  [
+            'id' => $contact->getId(),
+            'name' => $contact->getName(),
+            'firstName' => $contact->getFirstName(),
+            'email' => $contact->getEmail(),
+            'address' => $contact->getAddress(),
+            'phone' => $contact->getPhone(),
+            'age' => $contact->getAge(),
+        ];
+
+        return $this->json($data);
     }
 }
